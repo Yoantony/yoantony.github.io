123fccf951a3ea629db3bf3754be71d4347ca117
Yoantony <paratareas746@gmail.com>
2024-03-07 07:55:17 -0600

    cambio de js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const toggle = document.querySelector(".toggle")
 const navMenu = document.querySelector(".nav-Menu")
-toggle.addEventListener("click",()=>{
-    navMenu.classList.toggle("nav-menvi");
-});
+newFunction();
+function newFunction(){
+    toggle.addEventListener("click", () => {
+        navMenu.classList.toggle("nav-menvi");
+    });
+} 
